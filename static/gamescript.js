ae99d4c79166791ac7010e993ac6ae146e589fc0
Natsuki <haseganats@myvuw.ac.nz>
2020-01-14 12:43:18 +1300

    test

--- static/gamescript.js
+++ static/gamescript.js
@@ -1,36 +1,55 @@
 // Variables
 let wordArray = []
 let currentWord = ""
-let timeLimit = 0
+let timeLimit = 10
 let gameRunning = false
+let message = "my message"
+let inputWord = ""
 
 // Functions
 function Init() {
     wordArray = [
         "apple", "banana", "orange", "pineapple", "grape", "cherry", "mango"
     ]
-    timeLimit = 60 * 1000
-    $("#time").text(timeLimit/1000)
-}
-
-function GameStart(){
-    console.time("timer")
-    gameRunning = true
-
-    while($("#word-input").text() != currentWord){
-
-    }
-
+    $("#time").text(timeLimit)
+    $("#message").text(message)
 }
 
 $(document).ready(function () {
     Init()
-    currentWord = wordArray[Math.ceil(Math.random() * wordArray.length)]
+    let random = Math.floor(Math.random() * wordArray.length)
+    console.log(random)
+    currentWord = wordArray[random]
     $("#word-output").text(currentWord)
 })
 
+
+function setCountdownTimer(){
+    $("#time").text(timeLimit-=1)
+}
+
+function GameStart(){
+    gameRunning = true
+    setInterval(setCountdownTimer, 1000)
+}
+
 $(document).ready(function (){
-    $("#word-input").keypress(function(){
-        GameStart()
+    // Do not use keydown -- will not read first character!!
+    $("#word-input").keyup(function(){
+
+        inputWord = $("#word-input").val()
+
+        console.log(inputWord)
+        console.log(currentWord)
+
+        if(gameRunning) {
+            if (inputWord === currentWord) {
+                $("#message").text("You win")
+            }
+        }
+        else {
+            GameStart()
+            $("#message").text("Playing game")
+        }
     })
 })

18abe565668478feb467d233ae9b1fbdd2e4ecaa
Natsuki <haseganats@myvuw.ac.nz>
2020-01-15 12:28:01 +1300

    test

--- app.py
+++ app.py
@@ -4,12 +4,20 @@ import csv
 
 app = Flask(__name__)
 myWords = []
+myResults = []
+
+
+class gameResult:
+    def __init__(self, userName, wordCount, maxTime):
+        self.userName = userName
+        self.wordCount = wordCount
+        self.maxTime = maxTime
 
 
 def loadCSV():
     global myWords
     try:
-        myFile = open("random_text.csv")
+        myFile = open("random_words.csv")
         reader = csv.reader(myFile)
         myWords = list(reader)
         return myWords
@@ -18,20 +26,10 @@ def loadCSV():
         print("File not found\n")
 
     finally:
-        for word in myWords:
-            print(word)
+        print('Number of words loaded from CSV: %d' % (myWords[0].__len__()))
 
 
 loadCSV()
-
-print('encoded')
-jsonList = json.dumps(myWords)
-for word in jsonList:
-    print(word)
-print('decoded')
-decodeList = json.loads(jsonList)
-for word in decodeList:
-    print(word)
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -44,17 +42,30 @@ def login():
     return render_template('login.html')
 
 
+@app.route('/results', methods=['POST', 'GET'])
+def results():
+    return render_template('results.html', myResults=myResults)
+
+
 @app.route('/game', methods=['POST', 'GET'])
 def game():
+    global myResults, myWords
+
     if request.method == 'POST':
         if request.is_json:
-            print('got json')
-            data = json.loads(request.get_data())
-            print(data)
+            data_receive = json.loads(request.get_data())
+            print('Received JSON data_receive from web app')
+            print(data_receive)
+
+            myResult = gameResult(data_receive['userName'], data_receive['wordCount'], data_receive['maxTime'])
+            myResults.append(myResult)
+
+            print(myResults)
             return render_template('game.html')
         else:
-            print('not json')
+            print('Did not receive JSON data_receive')
     else:
+        # Send the word list to the web page in JSON format
         return render_template('game.html', myWords=json.dumps(myWords))
 
 
